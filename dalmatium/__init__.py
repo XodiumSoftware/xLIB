a4ffa2cafc3a547486f016af4d18cec8010104b1
illyrius666 <FitimQ@live.nl>
2024-07-05 08:30:57 +0000

    chore: Remove unused theme packages and constants

--- dalmatium/__init__.py
+++ dalmatium/__init__.py
@@ -3,7 +3,6 @@
 from dalmatium.database import Database
 from dalmatium.forger import Forger
 from dalmatium.reader import Reader
-from dalmatium.theme import Theme
 from dalmatium.wrapper import Wrapper
 
 
@@ -14,12 +13,10 @@ class Utils:
         db,
         forge,
         read,
-        theme,
         wrap,
     ) = (
         Database,
         Forger,
         Reader,
-        Theme,
         Wrapper,
     )
